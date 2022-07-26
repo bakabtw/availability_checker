ea25b11f9103bd2a38a627a5d04133193c014e0e
Eugene Konstantinov <me@knst.me>
2022-07-26 09:43:03 +0300

    Added DB Models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Methods(models.Model):
+    methods = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.methods
+
+
+class Hosts(models.Model):
+    name = models.TextField()
+    server = models.TextField(null=True, blank=True)
+    port = models.IntegerField()
+    method = models.ForeignKey(Methods, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
