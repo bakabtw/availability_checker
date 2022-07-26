3b4d3202694500e053143ca857f19e94cef1ce65
Eugene Konstantinov <me@knst.me>
2022-07-26 09:41:48 +0300

    Added the 'base' urls to the project

--- base/views.py
+++ base/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Hosts, Methods
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("Working!")
