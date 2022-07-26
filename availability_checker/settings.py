839d13259e80281e9cabcf80e617d71971669097
Eugene Konstantinov <me@knst.me>
2022-07-26 10:06:03 +0300

    Added template directory

--- availability_checker/settings.py
+++ availability_checker/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'availability_checker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
