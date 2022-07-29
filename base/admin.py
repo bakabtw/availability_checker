e92f3bf866914fa92e1f614bf13f3c216082dd4d
Eugene Konstantinov <me@knst.me>
2022-07-30 00:28:07 +0300

    Updated Models

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Host, Method
+from .models import Host, Method, Status
 
 admin.site.register(Host)
 admin.site.register(Method)
+admin.site.register(Status)
--- base/models.py
+++ base/models.py
@@ -11,13 +11,28 @@ class Method(models.Model):
 
 
 class Host(models.Model):
-    name = models.CharField(max_length=50)
-    server = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)  # Update pony ORM
+    description = models.CharField(max_length=150)
+    server = models.CharField(max_length=100)
     port = models.IntegerField()
     method = models.ForeignKey(Method, on_delete=models.SET_NULL, null=True)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)  # Update pony ORM
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+
+class Status(models.Model):
+    status = models.CharField(max_length=50)
+    response_code = models.IntegerField()
+    url = models.CharField(max_length=150)
+    method = models.CharField(max_length=50)
+    content_type = models.CharField(max_length=50)
+    content_length = models.IntegerField()
+    server_id = models.ForeignKey(Host, on_delete=models.SET_NULL, null=True)
+    timestamp = models.TimeField(auto_now=True)
+
+    def __str__(self):
+        return self.url
