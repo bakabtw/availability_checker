5c403ccf5072776b8d82fd9e9aaa97e90047c896
Eugene Konstantinov <me@knst.me>
2022-07-30 02:07:01 +0300

    Added initial support for Statuses

--- base/views.py
+++ base/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Host, Method
+from .models import Host, Status
 
 
 def home(request):
-    hosts = Host.objects.all()
-    context = {'hosts': hosts}
+    hosts = Host.objects.filter(owner__username='admin')
+    # statuses = Status.objects.filter(server_id=hosts)
+    statuses = Status.objects.filter(server_id=-1)  # TODO: update the line
+
+    for host in hosts:
+        # statuses = statuses | Status.objects.filter(server_id=host.id)
+        statuses = statuses | Status.objects.filter(server_id=host.id)
+
+    context = {'hosts': hosts, 'statuses': statuses}
 
     return render(request, 'base/home.html', context)
