b116f5cc2e38de892d7dce0a0b866098a4e18df2
Eugene Konstantinov <me@knst.me>
2022-07-30 02:06:27 +0300

    Updated Models

--- base/models.py
+++ base/models.py
@@ -3,25 +3,25 @@ from django.contrib.auth.models import User
 
 
 class Method(models.Model):
-    methods = models.CharField(max_length=15)
+    method = models.CharField(max_length=15)
     description = models.TextField()
 
     def __str__(self):
-        return self.methods
+        return self.method
 
 
 class Host(models.Model):
-    name = models.CharField(max_length=50)  # Update pony ORM
+    name = models.CharField(max_length=50)  # TODO: Update pony ORM
     description = models.CharField(max_length=150)
     server = models.CharField(max_length=100)
     port = models.IntegerField()
     method = models.ForeignKey(Method, on_delete=models.SET_NULL, null=True)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)  # Update pony ORM
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)  # TODO: Update pony ORM
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return str(self.id)
 
 
 class Status(models.Model):
@@ -35,4 +35,4 @@ class Status(models.Model):
     timestamp = models.TimeField(auto_now=True)
 
     def __str__(self):
-        return self.url
+        return str(self.id)
